6b21b6042ebaeb6a3be188fb97c11c2e92315beb
Idris Khenchil <idriskhenchil@gmail.com>
2022-12-30 18:21:12 -0500

    Create StringEnds.js

---
+++ StringEnds.js
@@ -0,0 +1,14 @@
+// Description
+
+// Complete the solution so that it returns true if the first
+// argument(string) passed in ends with the 2nd argument (also a string).
+
+// solution('abc', 'bc') // returns true
+// solution('abc', 'd') // returns false
+
+function solution(str, ending){
+     return str.slice((str.length - ending.length), (str.length)) === ending;
+}
+
+// Afterthought
+// Use .endsWith() method
