2f983a9822c598e96413278bd4d76af8af37b37d
Idris Khenchil <idriskhenchil@gmail.com>
2022-12-31 00:01:08 -0500

    Training

---
+++ BoolString.js
@@ -0,0 +1,10 @@
+// Convert a Boolean to a String
+
+// Description
+// Implement a function which convert the given boolean value into its string representation.
+
+function booleanToString(b){
+    return b ? 'true': 'false';
+}
+
+
