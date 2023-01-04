3322f054f6a33137d66a30e2179ff8b02854d551
Idris Khenchil <idriskhenchil@gmail.com>
2023-01-03 21:44:19 -0500

    Training

---
+++ DoubleArray.js
@@ -0,0 +1,13 @@
+// Beginner - Lost Without a Map
+
+// Given an array of integers, return a new array with each value doubled.
+// For example:
+// [1, 2, 3] --> [2, 4, 6]
+
+let items = [1, 2, 3];
+
+function maps(x){
+    return x.map(x => x*2);
+}
+
+console.log(maps(items))
